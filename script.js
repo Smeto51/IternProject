2a9a5ec5e0c9a2fcbe6f43da5ce00fd373205137
Smeto <smeto@vk.com>
2025-02-05 04:19:03 +0300

    JavaScript basics learned

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 "use strict";
-alert("Created by Smeto")
+/*let sum = (a,b) => {
+    let result = a + b;
+    return result;
+}
+console.log(sum(2,5));
+*/
+
+let userName = prompt("Введите имя", "Алиса");
+let isTeaWanted = confirm("Вы хотите чаю?");
+
+alert( "Посетитель: " + userName ); // Алиса
+alert( "Чай: " + isTeaWanted ); // true
+/*alert("После этого сообщения ждите ошибку");
+
+[1,2,"Pedic"].forEach(alert);
+
+/*let ask = (question, yes, no) => {
+    confirm(question) ? yes() : no()
+} 
+    ask ("Тебе есть 18?",
+        () => alert("o YES"),
+        () => alert("lox")
+    );
+let sum = (a, b) => a + b;
+console.log(sum(2,5))
+
+
+let age = prompt("Сколько Вам лет?", 18),
+    welcome = (age<18) ?
+    function() { alert('Hi');    } :
+    function() { alert("Hello"); };
+
+//let welcome = (age < 18) ?
+//  () => alert('Привет!') :
+//  () => alert("Здравствуйте!");
+
+welcome(); // Error: welcome is not defined
+// ...не работает*/
